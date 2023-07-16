f021807560213a15d6517ccc8fc5e067a70c6d7a
cyberomanov <romanoff_1@bk.ru>
2023-07-16 18:43:59 +0300

    v1.4 - config update

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ total_max_8192_games_per_address = 18
 
 # coinflip
 max_flip_count_per_session_in_range = (2, 4)
-flip_bet_variants_in_sui = [1]  # rec: [1]
+flip_bet_variants_in_sui = [1]  # rec: [1], def: [1, 2, 5, 10, 25, 50]
 
 # general
 sui_rpc = "https://sui-mainnet-rpc.allthatnode.com"
