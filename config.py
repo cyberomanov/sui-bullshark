b5e7cb20f1400387a72e97a3645167740128914b
cyberomanov <romanoff_1@bk.ru>
2023-07-17 22:27:15 +0300

    v1.4 - rpc change

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 # 8192
-total_max_8192_games_per_address_in_range = (2, 4)  # per the whole address
+total_max_8192_games_per_address_in_range = (10, 12)  # per the whole address
 
 # coinflip
 max_flip_count_per_session_in_range = (2, 4)  # per session
 flip_bet_variants_in_sui = [1]  # rec: [1], def: [1, 2, 5, 10, 25, 50]
 
 # general
-sui_rpc = "https://mainnet.suiet.app/"
+sui_rpc = "https://sui-rpc-mainnet.testnet-pride.com/"
 sleep_range_between_txs_in_sec = (7, 15)
 sleep_range_between_games_in_sec = (60, 180)
 start_threads_simultaneously = False
