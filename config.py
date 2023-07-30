f6ced7a7cb1981fd0db83742440e0240f36c410a
cyberomanov <romanoff_1@bk.ru>
2023-07-30 16:40:20 +0300

    v1.7 - transfer.py

--- config.py
+++ config.py
@@ -1,13 +1,16 @@
 # 8192
-total_max_8192_games_per_address_in_range = (4, 8)  # per the whole address
+total_max_8192_games_per_address_in_range = (30, 32)  # per the whole address
 
 # coinflip
 max_flip_count_per_session_in_range = (2, 4)  # per session
 flip_bet_variants_in_sui = [1]  # rec: [1], def: [1, 2, 5, 10, 25, 50]
 
+# transfer
+value_to_leave_in_sui = (0.1, 0.2)
+
 # general
-sui_rpc = "https://sui-rpc-mainnet.testnet-pride.com/"
-sleep_range_between_txs_in_sec = (10, 20)
+sui_rpc = "https://mainnet.sui.rpcpool.com/"
+sleep_range_between_txs_in_sec = (5, 15)
 sleep_range_between_games_in_sec = (60, 180)
-start_threads_simultaneously = False
+start_threads_simultaneously = True
 check_derivation_paths = False
